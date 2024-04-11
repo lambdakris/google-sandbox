358985c09a70ba46f3eabc594d39a8402181d185
Kristoffer Garcia <lambdakris@gmail.com>
2024-04-11 11:22:32 -0600

    correct ci/cd platform
    correct issue with github account permissions so that the hello ci/cd can provision the hello iac

--- platform/infra/cicd.tf
+++ platform/infra/cicd.tf
@@ -35,3 +35,61 @@ resource "google_service_account_iam_member" "github_workload" {
     role = "roles/iam.workloadIdentityUser"
     member = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github.name}/attribute.repository/lambdakris/google-sandbox"
 }
+
+resource "google_project_iam_custom_role" "github" {
+    role_id = "github"
+    title = "Github Role"
+    stage = "ALPHA"
+    permissions = [
+        "storage.objects.list",
+        "storage.objects.get",
+        "storage.objects.create",
+        "storage.objects.delete",
+        "storage.objects.update",
+        "run.services.get",
+        "run.services.create",
+        "run.services.delete",
+        "run.services.update",
+        "run.services.getIamPolicy",
+        "run.services.setIamPolicy",
+        "run.jobs.get",
+        "run.jobs.create",
+        "run.jobs.delete",
+        "run.jobs.update",
+        "run.jobs.getIamPolicy",
+        "run.jobs.setIamPolicy",
+        "run.operations.get",
+        "artifactregistry.repositories.create",
+        "artifactregistry.repositories.createTagBinding",
+        "artifactregistry.repositories.delete",
+        "artifactregistry.repositories.deleteArtifacts",
+        "artifactregistry.repositories.deleteTagBinding",
+        "artifactregistry.repositories.downloadArtifacts",
+        "artifactregistry.repositories.get",
+        "artifactregistry.repositories.getIamPolicy",
+        "artifactregistry.repositories.list",
+        "artifactregistry.repositories.listEffectiveTags",
+        "artifactregistry.repositories.listTagBindings",
+        "artifactregistry.repositories.readViaVirtualRepository",
+        "artifactregistry.repositories.setIamPolicy",
+        "artifactregistry.repositories.update",
+        "artifactregistry.repositories.uploadArtifacts",
+        "artifactregistry.tags.create",
+        "artifactregistry.tags.delete",
+        "artifactregistry.tags.get",
+        "artifactregistry.tags.list",
+        "artifactregistry.tags.update",
+        "artifactregistry.versions.delete",
+        "artifactregistry.versions.get",
+        "artifactregistry.versions.list",
+        "iam.serviceAccounts.actAs",
+        "iam.serviceAccounts.get",
+        "iam.serviceAccounts.list",
+    ]
+}
+
+resource "google_project_iam_member" "github" {
+    project = var.project
+    role = google_project_iam_custom_role.github.id
+    member = "serviceAccount:${google_service_account.github.email}"
+}
